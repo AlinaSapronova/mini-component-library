9271642a79233e9e8b34a6448946498e637ee27e
alina <sapron4ik93@gmail.com>
2024-02-21 15:53:18 -0600

    progress bar done

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,76 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--height": 8 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  medium: {
+    "--height": 12 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  large: {
+    "--height": 16 + "px",
+    "--borderRadius": 4 + "px",
+  },
+}
+
+const WRAPPER_SIZES = {
+  small: {
+    "--height": 8 + "px",
+    "--radius": 4 + "px",
+    "--padding": 0 +'px',
+  },
+  medium: {
+    "--height": 12 + "px",
+    "--radius": 4 + "px",
+    "--padding": 0 +'px',
+  },
+  large: {
+    "--height": 24 + "px",
+    "--radius": 8 + "px",
+    "--padding": 4 +'px',
+  },
+}
+
+
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+const barSize = SIZES[size]
+const wrapperStyle = WRAPPER_SIZES[size]
+
+  return (
+  <Wrapper style={wrapperStyle}>
+     <ProgressTrack value={value} size={size}style={barSize}/>
+  </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+ width: 370px;
+ height: var(--height);
+ border-radius: var(--radius);
+ padding: var(--padding);
+ background-color: ${COLORS.transparentGray35};
+ box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35}
+ 
+`;
+
+const ProgressTrack = styled.div`
+   background-color: ${COLORS.primary};
+   width: ${({value, size}) => 
+   size === "large" 
+   ? (value / 100) * (370 - 8)
+   :(value / 100) * (370)}px;
+   height: var(--height);
+   border-radius: ${({value}) => 
+     (value / 100) * 370 === 370 
+     ? "var(--borderRadius)"
+     : "var(--borderRadius) 0 0 var(--borderRadius)"
+    };
+
+`;
 
 export default ProgressBar;
